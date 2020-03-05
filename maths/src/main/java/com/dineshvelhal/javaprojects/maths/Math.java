773a93c472253f6812c62c4be46179f538fb968f
Tal Yitzhak <talyitzhak100@gmail.com>
2020-03-05 19:19:45 +0200

    Update Math.java

--- maths/src/main/java/com/dineshvelhal/javaprojects/maths/Math.java
+++ maths/src/main/java/com/dineshvelhal/javaprojects/maths/Math.java
@@ -11,7 +11,7 @@ package com.dineshvelhal.javaprojects.maths;
 public class Math 
 {
     public int add(int a, int b) {
-    	return a + b;
+    	return a + b + 1;
     }
     
     public int subtract(int a, int b) {
